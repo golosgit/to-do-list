4483005658733d2edb3c632d4540e5f9d07bedb8
Grzegorz Gołos <golosgit@gmail.com>
2022-06-16 13:50:46 +0200

    adding new task; checking and showing status of every task

--- js/script.js
+++ js/script.js
@@ -1,30 +1,54 @@
 {
   const taskList = [
     {
-      task: "zadanie 1",
+      job: "zadanie 1",
       status: true,
     },
     {
-      task: "zadanie 2",
+      job: "zadanie 2",
       status: false,
     },
     {
-      task: "zadanie 3",
+      job: "zadanie 3",
       status: true,
     },
   ];
+
+  const addNewJob = (newTaskListJob) => {
+    taskList.push({
+      job: newTaskListJob,
+    });
+    render();
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+
+    const newTaskListJob = document.querySelector(".js-newJob").value.trim();
+
+    if (newTaskListJob === "") {
+      return;
+    }
+
+    addNewJob(newTaskListJob);
+  };
 
   const render = () => {
     let htmlListString = "";
 
     for (const task of taskList) {
-      htmlListString += `<li>${task.task}</li>`;
+      htmlListString += `<li 
+      ${task.status ? 'class="list__itemLineThrough"' : ""}>${task.job}</li>`;
     }
     document.querySelector(".js-taskList").innerHTML = htmlListString;
   };
 
   const init = () => {
     render();
+
+    const form = document.querySelector(".js-form");
+
+    form.addEventListener("submit", onFormSubmit);
   };
 
   init();
